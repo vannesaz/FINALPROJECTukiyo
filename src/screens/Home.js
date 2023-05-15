cadb0b130c92d8a81af8bdfa4289465088714e91
LuckyGear <geraldpoluan@gmail.com>
2023-05-15 12:20:24 +0800

    tambah back button

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,28 +1,51 @@
-import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { BackGround } from '../assets'
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-native/no-inline-styles */
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+import {BackGround} from '../assets';
 
 const Home = ({navigation}) => {
   return (
-    <ScrollView style = {{ backgroundColor : "#CED9DF"}}>
-      <View style = {{backgroundColor : "#1F2F22", paddingHorizontal : 28, paddingVertical : 18, borderBottomLeftRadius : 25, borderBottomRightRadius : 25 }}>
-        <Text style = {{color : "white", fontSize : 20}}>
-            Hi. ukiyo here.
+    <ScrollView style={{backgroundColor: '#CED9DF'}}>
+      <View
+        style={{
+          backgroundColor: '#1F2F22',
+          paddingHorizontal: 28,
+          paddingVertical: 18,
+          borderBottomLeftRadius: 25,
+          borderBottomRightRadius: 25,
+        }}>
+        <Text style={{color: 'white', fontSize: 20}}>Hi. ukiyo here.</Text>
+        <Text style={{color: 'white', fontSize: 15}}>
+          let's create your own little world to relax.
         </Text>
-        <Text style = {{color : "white", fontSize : 15}}>
-            let's create your own little world to relax.
-        </Text>
-        <Image source={BackGround} style = {{width : "100%",height : 500, borderRadius : 25, marginTop : 20, }}/>
-        <TouchableOpacity style = {{alignSelf : 'center', backgroundColor : '#19A7CE', paddingHorizontal : 5, marginTop : 10, borderRadius : 5}} onPress = {()=>navigation.navigate("CSS")}>
-            <Text style = {{color : 'white'}}>
-                Start
-            </Text>
+        <Image
+          source={BackGround}
+          style={{width: '100%', height: 500, borderRadius: 25, marginTop: 20}}
+        />
+        <TouchableOpacity
+          style={{
+            alignSelf: 'center',
+            backgroundColor: '#19A7CE',
+            paddingHorizontal: 5,
+            marginTop: 10,
+            borderRadius: 5,
+          }}
+          onPress={() => navigation.navigate('CSS')}>
+          <Text style={{color: 'white'}}>Start</Text>
         </TouchableOpacity>
       </View>
     </ScrollView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/screens/Play.js
+++ src/screens/Play.js
@@ -1,37 +1,74 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
-import { FotoPlay } from '../assets'
-import { Playbutton, Previous, Next, Pause } from '../assets/icons'
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import {FotoPlay} from '../assets';
+import {Playbutton, Previous, Next, Pause} from '../assets/icons';
+import TrackPlayer from 'react-native-track-player';
 
 const Play = () => {
-    const [Play, setPlay] = useState (false)
-    return ( 
-    <View style = {{backgroundColor : "#CED9DF", flex : 1}}>
-      <View style = {{backgroundColor : "#1F2F22", height : "95%", borderBottomLeftRadius : 20, borderBottomRightRadius : 20, paddingHorizontal : 30, alignItems : 'center', paddingVertical : 20}}>
-            <View style = {{alignSelf : "flex-start"}}>
-            <Text style = {{fontFamily : "OriginalSurfer-Regular", color : 'white', fontSize : 20}}>Default Playlist Name</Text>
-            <Text style = {{fontFamily : "OriginalSurfer-Regular", color : 'white'}}>Music Title - Composer</Text>
-            </View>
+  const [Play, setPlay] = useState(false);
+  const setupPlayer = async () => {
+    await TrackPlayer.setupPlayer();
 
-        <View style = {{borderRadius : 25, overflow : 'hidden', height : "70%", marginTop : 35}}>
-            <Image source={FotoPlay} style = {{flex : 1}}/>
+    await TrackPlayer.add();
+  };
+  return (
+    <View style={{backgroundColor: '#CED9DF', flex: 1}}>
+      <View
+        style={{
+          backgroundColor: '#1F2F22',
+          height: '95%',
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20,
+          paddingHorizontal: 30,
+          alignItems: 'center',
+          paddingVertical: 20,
+        }}>
+        <View style={{alignSelf: 'flex-start'}}>
+          <Text
+            style={{
+              fontFamily: 'OriginalSurfer-Regular',
+              color: 'white',
+              fontSize: 20,
+            }}>
+            Default Playlist Name
+          </Text>
+          <Text style={{fontFamily: 'OriginalSurfer-Regular', color: 'white'}}>
+            Music Title - Composer
+          </Text>
         </View>
-        <View style = {{flexDirection : 'row', justifyContent : 'space-evenly', width : "70%", marginTop : 50}}>
-            <TouchableOpacity>
-                <Previous/>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => Play === true ? setPlay (false) : setPlay (true)}>
-              {  Play ? <Pause/> : <Playbutton/> }
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Next/>
-            </TouchableOpacity>
+
+        <View
+          style={{
+            borderRadius: 25,
+            overflow: 'hidden',
+            height: '70%',
+            marginTop: 35,
+          }}>
+          <Image source={FotoPlay} style={{flex: 1}} />
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            width: '70%',
+            marginTop: 50,
+          }}>
+          <TouchableOpacity>
+            <Previous />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => (Play === true ? setPlay(false) : setPlay(true))}>
+            {Play ? <Pause /> : <Playbutton />}
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Next />
+          </TouchableOpacity>
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Play
+export default Play;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
